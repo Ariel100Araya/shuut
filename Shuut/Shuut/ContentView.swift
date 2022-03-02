029ef8fee00d2383b07a1495185286058b5b0662
Ariel Araya-Madrigal <58918901+Ariel100Araya@users.noreply.github.com>
2022-03-01 20:02:50 -0500

    stuff

---
+++ Shuut/Shuut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shuut
+//
+//  Created by Ariel Araya-Madrigal on 2/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shuut/Shuut/ShuutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuutApp.swift
+//  Shuut
+//
+//  Created by Ariel Araya-Madrigal on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ShuutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
